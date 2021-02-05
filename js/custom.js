5a64e345df43938cf30cfa2a9b2189b51d8fdde8
MaxSimakov <Simakov.maxim82@gmail.com>
2021-02-06 00:09:14 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+const $ = jQuery;
+
+function eventHandler() {
+		
+	//sQusetions js
+	let qItem = document.querySelectorAll(".q-item-js");
+	if(qItem) {
+		qItem.forEach(function (el) {
+			el.addEventListener('click', function () {
+				let qItem = document.querySelectorAll('.q-item-js');
+				let self = this;
+
+				for (let item of qItem) {
+					let currContent = item.querySelector('.q-content-js');
+
+					if (item === self) {
+						item.classList.toggle('active');
+						currContent.classList.toggle('active');
+					}
+					else {
+						item.classList.remove('active');
+						currContent.classList.remove('active');
+					}
+				}
+			})
+		});
+	}
+
+	let mobileToggle = document.querySelector('.mainMenu__mobile');
+	if(mobileToggle) {
+		mobileToggle.addEventListener('click', function () {
+			document.querySelector('.mainMenu__items').classList.toggle('active');
+			this.classList.toggle('active');
+		});
+	};
+};
+
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+};
+
